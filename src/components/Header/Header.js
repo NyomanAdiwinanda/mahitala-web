47b96d50242181e7fc97e4e829dbc0b328699601
Nyoman Adiwinanda <nyoman.adi16@gmail.com>
2020-10-10 14:46:22 +0700

    adding static header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import NotesIcon from '@material-ui/icons/Notes';
+import SearchIcon from '@material-ui/icons/Search';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import LogoMahitala from './LogoMahitala.png';
+import './Header.css';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#ECF0F1',
+        },
+    },
+});
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div className="header-component">
+                <div className="menu">
+                    <ThemeProvider theme={theme}>
+                        <NotesIcon fontSize="large" color="primary" />
+                    </ThemeProvider>
+                </div>
+                <img src={LogoMahitala} alt="Logo Mahitala" />
+                <div className="search">
+                    <ThemeProvider theme={theme}>
+                        <SearchIcon fontSize="large" color="primary" />
+                    </ThemeProvider>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Header;
