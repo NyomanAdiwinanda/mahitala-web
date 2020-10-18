cf05e5cd0c362944b8159d5ea3736cebf8fb4e71
MochRafi007 <rafisetiadipura@gmail.com>
2020-10-18 22:56:56 +0700

    Rekam Jejak
    1. Menambahkan file foto peta indonesia dan background black
    2. Membuat text ing pada bagian rekam jejak

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import Featured from './components/Featured/Featured';
+import RekamJejak from './components/RekamJejak/RekamJejak';
 import './App.css';
 
 function App() {
@@ -12,6 +13,9 @@ function App() {
       <div className="main-section">
         <div className="featured-section">
           <Featured />
+        </div>
+        <div className="rekam-jejak-section">
+          <RekamJejak />
         </div>
       </div>
     </div>
